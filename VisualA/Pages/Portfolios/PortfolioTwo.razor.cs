e8b7d4727a0e2cd73d95b4d57e2e3cbd937d8286
kimdohyun5 <musku@naver.com>
2024-12-10 16:19:49 +0900

    portfolio 내용 component화

---
+++ VisualA/Pages/Portfolios/PortfolioTwo.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualA.Pages.Portfolios
+{
+    public partial class PortfolioTwo
+    {
+    }
+}
